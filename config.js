5891e16c23daf1093451bf3e9253a413c42d12b1
Tim Arney <tim@line37.com>
2017-10-22 13:34:51 -0400

    update device screen

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const numQuestions = 5;
-export const welcomeTitle = "Welcome to the Triva Challenge!";
+export const numQuestions = 10;
+export const welcomeTitle = "Welcome to the Trivia Challenge!";
 export const errorText = "Failed to load data, please try again later.";
